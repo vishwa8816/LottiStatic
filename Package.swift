efd5f596866304330e09d7873f66f55b97cfdae3
Vishwa Patel <vishwa.patel@medibank.com.au>
2023-08-06 09:10:20 +1000

    Updated Package info: Added binary Target, and creating static library.

--- Package.swift
+++ Package.swift
@@ -5,24 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "LottiStatic",
+    platforms: [.iOS("14.0")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LottiStatic",
-            targets: ["LottiStatic"]),
+            type: .static,
+            targets: ["LottiStatic", "Lottie"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LottiStatic",
             dependencies: []),
-        .testTarget(
-            name: "LottiStaticTests",
-            dependencies: ["LottiStatic"]),
+        .binaryTarget(name: "Lottie",
+                      url: "https://github.com/airbnb/lottie-ios/releases/download/4.2.0/Lottie.xcframework.zip",
+                      checksum: "4db3dee208f6213e5c1681f2314c7ed96d466d9b9adfe5cd0030309515075443")
     ]
 )
